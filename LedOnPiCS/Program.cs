20b7b56ad0d26e9a2d174b506a4c916060c1b078
Maarten Van De Bospoort <maartenb@microsoft.com>
2018-01-26 13:46:28 -0800

    adding a few more project directories

---
+++ LedOnPiCS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LedOnPiCS
+{
+    using Unosquare.RaspberryIO;
+    using Unosquare.RaspberryIO.Gpio;
+
+    class Program
+    {
+        private static GpioPin pin = Pi.Gpio.Pin03;
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            pin.PinMode = GpioPinDriveMode.Output;
+
+            for (int i = 0; i < 10; i++)
+                Blink(500, 1000);
+        }
+
+        private static void Blink(int on, int cycle)
+        {
+            pin.Write(true);   
+            System.Threading.Thread.Sleep(on);
+            pin.Write(true);   
+            System.Threading.Thread.Sleep(cycle-on);
+        }
+    }
+}
--- hello.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-# Hello World
-for n in range(1,10):
-    print('Hello World! '+str(n ))
