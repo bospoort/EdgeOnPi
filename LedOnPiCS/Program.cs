d03054748e09766731bb7dc379ecaad699edc518
Maarten Van De Bospoort <maartenb@microsoft.com>
2018-01-26 16:49:05 -0800

    turning on LED from .NET core on Pi

--- LedOnPiCS/Program.cs
+++ LedOnPiCS/Program.cs
@@ -4,26 +4,45 @@ namespace LedOnPiCS
 {
     using Unosquare.RaspberryIO;
     using Unosquare.RaspberryIO.Gpio;
-
     class Program
     {
-        private static GpioPin pin = Pi.Gpio.Pin03;
- 
+        private static GpioPin pin;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            pin = Pi.Gpio.Pin09; //Pin03 appears to be BCM 22 and HeaderPin 25
             pin.PinMode = GpioPinDriveMode.Output;
 
             for (int i = 0; i < 10; i++)
-                Blink(500, 1000);
+            {
+                Blink(pin, 500, 1000);
+            }
+
+            // int counter = 0;
+            // foreach (var pin in Pi.Gpio.Pins)
+            // {
+            //     System.Console.WriteLine("Counter: " + counter++);
+            //     pin.PinMode = GpioPinDriveMode.Output;
+            //     System.Console.WriteLine("Pin: " + pin.PinNumber);
+            //     System.Console.WriteLine("HeaderPinNumber: " + pin.HeaderPinNumber);
+            //     System.Console.WriteLine("BCMPinNumber: " + pin.BcmPinNumber);
+
+            //     for (int i = 0; i < 10; i++)
+            //     {
+            //         Blink(pin, 500, 1000);
+            //     }
+            // }
         }
 
-        private static void Blink(int on, int cycle)
+        private static void Blink(GpioPin pin, int on, int cycle)
         {
-            pin.Write(true);   
+            System.Console.WriteLine("Turning LED to true");
+            pin.Write(true);
             System.Threading.Thread.Sleep(on);
-            pin.Write(true);   
-            System.Threading.Thread.Sleep(cycle-on);
+            System.Console.WriteLine("Turning LED to false");
+            pin.Write(false);
+            System.Threading.Thread.Sleep(cycle - on);
         }
     }
 }
