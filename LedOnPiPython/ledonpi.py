1a01c8dfaa1fbe6ec2a8268f71feb038a95279bb
Maarten Van De Bospoort <maartenb@microsoft.com>
2018-01-30 16:15:36 -0800

    modifying docker files to run on pi

--- LedOnPiPython/ledonpi.py
+++ LedOnPiPython/ledonpi.py
@@ -4,8 +4,11 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(3, GPIO.OUT)
-print ("LED On")
-GPIO.output(3, GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(3, GPIO.LOW)
+for x in range(0,4):
+    print ("LED On")
+    GPIO.output(3, GPIO.HIGH)
+    time.sleep(1)
+    print "LED off"
+    GPIO.output(3, GPIO.LOW)
+    time.sleep(1)
+print "Done. Bye"
